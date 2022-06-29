ccdd169207722ff0bca67449640cc17a3761e070
KeerthiTejaswi <106312510+KeerthiTejaswi@users.noreply.github.com>
2022-06-29 05:59:26 +0530

    first commit

---
+++ Typescript/practice.ts
@@ -0,0 +1,8 @@
+type objType={ name:string,age:number,email:string,gender:string};
+const person:objType={
+  name:"raju",
+  age:20,
+  gender:"male",
+  email:'xyz@test.com'
+};
+console.log(person);
