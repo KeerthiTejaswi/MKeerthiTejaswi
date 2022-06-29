0451182b49d76a0c61e8a7d759264daa1f498a69
KeerthiTejaswi <106312510+KeerthiTejaswi@users.noreply.github.com>
2022-06-29 06:31:22 +0530

    first commit

---
+++ Typescript/loops.ts
@@ -0,0 +1,24 @@
+for(var i=1;i<=3;i++) {
+    console.log(i);
+    if(i==2) {
+        continue;
+    }
+    else {
+        break;
+    }
+}
+var x:number=3;
+while(x<5) {
+console.log(x);
+x++;
+}
+var y:number=2;
+do {
+    console.log(y);
+    y++;
+    if(y==3) {
+        break;
+    
+    }
+
+    } while(y<5);
