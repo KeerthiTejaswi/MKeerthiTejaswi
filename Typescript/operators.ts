b22a20500de4b913771f5ce74831928d1ad2adc8
KeerthiTejaswi <106312510+KeerthiTejaswi@users.noreply.github.com>
2022-07-01 05:38:00 +0530

    second commit

--- Typescript/operators.ts
+++ Typescript/operators.ts
@@ -11,8 +11,16 @@ function multiplyNumbers(x:number,y:number) {
 var mul:number=multiplyNumbers(3,5);
 console.log('multiply of two numbers is:'+mul);
 
+
 function divNumbers(i:number,j:number) {
   return i/j;
 }
 var division:number=divNumbers(4,2) ;
 console.log('division of two numbers is :'+division);
+
+
+function subNumbers(k:number,m:number) {
+  return k-m;
+}
+var subtraction:number=subNumbers(3,2);
+console.log('subtraction of two numbers is :'+subtraction);
