aed94fe34919b7113193821513bed53f6cb857b8
KeerthiTejaswi <106312510+KeerthiTejaswi@users.noreply.github.com>
2022-07-01 15:40:54 +0530

    second commit

--- Typescript/Interface.ts
+++ Typescript/Interface.ts
@@ -1,3 +1,4 @@
+// Using Interface for Rectangle
 interface Rectangle {
     height:number,
     width:number
@@ -12,3 +13,31 @@ const MyRectangle:myRectangle={
     color:"blue"
 };
 console.log(MyRectangle);
+
+// Using Interface for Square
+interface Square {
+    side:number
+}
+interface mySquare extends Square {
+    color:string
+}
+const MySquare:mySquare={
+    side:20,
+    color:"green"
+};
+console.log(MySquare);
+
+// Using Interface for Triangle
+interface Triangle {
+    base:number,
+    height:number
+}
+interface myTriangle extends Triangle {
+    color:string
+}
+const MyTriangle:myTriangle={
+    base:10,
+    height:20,
+    color:"red"
+};
+console.log(MyTriangle);
