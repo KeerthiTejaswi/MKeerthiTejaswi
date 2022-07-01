b39ca986e7d5b87b46fc88abfac862a265e008b7
KeerthiTejaswi <106312510+KeerthiTejaswi@users.noreply.github.com>
2022-07-01 17:09:51 +0530

    second commit

--- Typescript/practice.ts
+++ Typescript/practice.ts
@@ -1,3 +1,4 @@
+
 type objType={ name:string,age:number,email:string,gender:string};
 const person:objType={
   name:"raju",
@@ -6,3 +7,16 @@ const person:objType={
   email:'xyz@test.com'
 };
 console.log(person);
+
+// Access Modifiers
+class Girl {
+    private  name:string;
+public constructor(name:string){
+    this.name=name;
+}
+public fullName():string {
+    return this.name;
+}
+}
+const girl=new Girl("rani");
+console.log(girl.fullName());
