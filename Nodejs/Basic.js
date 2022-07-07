f2059171450b0155f362ddc90d10a73d77829a6e
KeerthiTejaswi <106312510+KeerthiTejaswi@users.noreply.github.com>
2022-07-08 04:47:22 +0530

    second commit

--- Nodejs/Basic.js
+++ Nodejs/Basic.js
@@ -1,5 +1,7 @@
 var http = require('http');
+// create a server object
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hii! How are you');
-}).listen(8080);
+    // writing a response to the client and end the response
+}).listen(8080); // the server object listens on port 8080
