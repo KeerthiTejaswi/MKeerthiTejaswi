a15aa09cf7bf175b40280e62cb0d6e79bd720929
KeerthiTejaswi <106312510+KeerthiTejaswi@users.noreply.github.com>
2022-07-09 14:45:28 +0530

    updated

--- Nodejs/DateTime.js
+++ Nodejs/DateTime.js
@@ -1,7 +1,7 @@
-var http=require('http');
+var http=require('http'); // we use the require() function to include the modules
 var date=require('./firstmodule');
 http.createServer(function (req,res) {
     res.writeHead(200,{'content-type':'text/html'});
     res.write("the date and time are:"+date.DateTime());
     res.end();
-}).listen(8080);
+}).listen(8080); // we will be getting output as the date and time are: Sat Jul 09 2022 14:54:15 GMT+0530(India Standard Time)
