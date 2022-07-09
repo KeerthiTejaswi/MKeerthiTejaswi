469f55b2fbca466246a8762fd44e7945eaad69b8
KeerthiTejaswi <106312510+KeerthiTejaswi@users.noreply.github.com>
2022-07-09 14:40:58 +0530

    updated

--- Nodejs/Basic.js
+++ Nodejs/Basic.js
@@ -1,7 +1,7 @@
-var http = require('http');
+var http = require('http'); // we use the require() function to include the modules
 // create a server object
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hii! How are you');
-    // writing a response to the client and end the response
+    res.end('Hii! How are you'); // writing a response to the client and end the response
 }).listen(8080); // the server object listens on port 8080
+// we will be getting the output as 'Hii! How are you'.
